fd6ad38d2179c89114600aa340914d6ed517c4c3
Sandrafongshurui <sandrafong90@hotmail.com>
2022-10-27 21:27:40 +0800

    fix: delpoy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/bookshelf/loans" element={<BookshelfLoans />} />
         <Route path="/bookshelf/loans/:id/read/:page" element={<EpubReader />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </div>
   );
