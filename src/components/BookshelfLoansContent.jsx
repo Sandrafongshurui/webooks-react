87ab6a3816dba1355197567c4481f8fffff2a92c
Sandrafongshurui <[sandrafong90@hotmail.com]>
2022-11-01 20:35:48 +0800

    fix: httpS

--- src/components/BookshelfLoansContent.jsx
+++ src/components/BookshelfLoansContent.jsx
@@ -23,7 +23,7 @@ export const BookshelfLoansContent = () => {
   useEffect(() => {
     const fetchApi = async () => {
       const res = await axios.get(
-        `httpS://${process.env.REACT_APP_SERVER_URL}/api/v1/loans`,
+        `https://${process.env.REACT_APP_SERVER_URL}/api/v1/loans`,
         {
           headers: {
             "Content-Type": "application/json",
